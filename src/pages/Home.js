492eefdd3122d07713dcb5c6057ca502ed105534
Jamaar Green <greenjamaar@gmail.com>
2023-03-08 20:55:03 -0700

    first commit

---
+++ src/pages/Home.js
@@ -0,0 +1,22 @@
+import { useState } from "react";
+import React from 'react';
+import { Box } from "@mui/material";
+
+import Exercises from "../components/Exercises";
+import SearchExercises from "../components/SearchExercises";
+import HeroBanner from "../components/HeroBanner";
+
+const Home = () => {
+    const [bodyPart, setBodyPart] = useState('all');
+    const [exercises, setExercises] = useState([]);
+    
+    return (
+      <Box>
+        <HeroBanner />
+        <SearchExercises setExercises={setExercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+        <Exercises setExercises={setExercises} exercises={exercises} bodyPart={bodyPart} setBodyPart={setBodyPart} />
+      </Box>
+    );
+  };
+  
+  export default Home;
